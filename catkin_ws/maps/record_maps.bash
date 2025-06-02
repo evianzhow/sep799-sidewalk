3c32d69879c46a651eb9c61a8fcccb09439a61f1
Yifei Zhou <4608342+evianzhow@users.noreply.github.com>
2025-06-01 23:26:32 -0400

    Added simulation and real device mapping launch file

---
+++ catkin_ws/maps/record_maps.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+# https://raw.githubusercontent.com/UM-ARM-Lab/husky_stereo_nav/refs/heads/master/maps/record_maps.bash
+read -p "Enter map name: " mapName
+if [ -f $mapName.db ];
+then
+echo "This map already exists. Are you sure you want to overwrite it? y/n"
+while [ true ] ; do
+read -s -N 1 -t 1 key
+if [[ $key == $'y' ]];
+then
+rosrun map_server map_saver -f $mapName map:=/rtabmap/grid_map
+echo "Map file has been overwritten. Press enter once launch file is killed"
+break
+elif [[ $key != $'y' && $key != '' ]];
+then
+echo "Map file was not overwritten."
+exit 0;
+fi
+done
+else
+rosrun map_server map_saver -f $mapName map:=/rtabmap/grid_map
+echo "Press enter once launch file is killed"
+fi
+while [ true ] ; do
+read -s -N 1 -t 1 key
+if [[ $key == $'\x0a' ]];
+then
+cp ~/.ros/rtabmap.db $mapName.db
+exit 0;
+fi
+done
